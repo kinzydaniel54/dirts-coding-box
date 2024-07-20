4f9d2e6d08f55e7848ec78e0f5d3656ab88b07d5
kinzydaniel54 <kinzydaniel54@icloud.com>
2024-07-20 19:05:45 -0400

    y

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print("hello, hey fools Coding tempple Navigators!")
+    print("hello, hey  Coding tempple Navigators!")
 
 if __name__ =="main":
     greet_coding_temple
