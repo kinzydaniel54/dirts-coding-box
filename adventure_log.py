f3e8c3e04866d1ecf5053e5173ab0a70c6bf6da8
kinzydaniel54 <kinzydaniel54@icloud.com>
2024-07-20 20:43:35 -0400

    adventure_log.py

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,10 @@
 def greet_coding_temple():
+<<<<<<< Updated upstream
     print("hello, hey fools Coding tempple Navigators!")
+=======
+    print("hello, this is where it starts  Coding tempple Navigators!")
+>>>>>>> Stashed changes
 
 if __name__ =="main":
-    greet_coding_temple
+    greet_coding_templeq
+    
