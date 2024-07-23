b2eabe1584d444f10e74f1d179ea1bbccf62b5c1
kinzydaniel54 <kinzydaniel54@icloud.com>
2024-07-22 23:09:25 -0400

    y

--- adventure_log.py
+++
@@ -1,6 +0,0 @@
-def greet_coding_temple():
-    print("hello, hey fools Coding tempple Navigators!")
-
-if __name__ =="main":
-    greet_coding_temple
-    
--- readme.md
+++
@@ -1,5 +0,0 @@
-# my-coding-temple-ship-log
-
-author: coding temple
-
-url: www.codingtemple.com
